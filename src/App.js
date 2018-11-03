241fcc0e3892a66e3ac1966b1d71d062c0d07e52
DenisBeck <beckass86@gmail.com>
2018-11-03 13:31:17 +0500

    installed react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Route, Switch} from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -13,8 +14,10 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          {this.state.show ? <BurgerBuilder /> : null}
-          <Checkout />
+          <Switch>
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </div>
     );
