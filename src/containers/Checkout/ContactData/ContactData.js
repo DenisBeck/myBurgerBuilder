8345a0d7a0c87c8f16f3588a1cef9f75681eea49
DenisBeck <beckass86@gmail.com>
2018-11-09 14:56:20 +0500

    combining reducers

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -81,7 +81,7 @@ class ContactData extends Component {
                 valid: false,
                 touched: false
             },
-            delveryMethod: {
+            deliveryMethod: {
                 elementType: 'select',
                 elementConfig: {
                    options: [
@@ -198,9 +198,9 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients,
-        price: state.totalPrice,
-        loading: state.loading
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        loading: state.order.loading
     };
 };
 
